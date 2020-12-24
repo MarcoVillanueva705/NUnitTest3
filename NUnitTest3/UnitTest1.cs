d8c086e07d05429ba25a2016c55b30fd586296d3
marco villanueva <villanuevamarco98@gmail.com>
2020-12-24 14:27:21 -0700

    Add project files.

---
+++ NUnitTest3/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class StringCalculator_UnitTests
+    {
+        [Test]
+        //method name, scenario under which testing, & expected outcome
+        public void UnitUnderTest_Scenario_ExpectedOutcome()
+        {
+            Assert.Pass();
+        }
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+    }
+}
