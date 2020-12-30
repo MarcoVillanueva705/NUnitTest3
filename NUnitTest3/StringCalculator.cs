26ecc4863c80da053c52ccdca939afc3bb0f9600
marco villanueva <villanuevamarco98@gmail.com>
2020-12-29 20:46:58 -0700

    Add method req more refactoring

--- NUnitTest3/StringCalculator.cs
+++ NUnitTest3/StringCalculator.cs
@@ -7,10 +7,24 @@ namespace Tests
         public int Add(string input)
         {
             //solution for test one to pass
+            //if the input is null or a blank string (like line 21 in Unit test)
+            //if (string.IsNullOrEmpty(input))
+            //    return 0;
+            //return int.Parse(input);
+
             if (string.IsNullOrEmpty(input))
+            {
                 return 0;
-            return int.Parse(input);
-            
+            }
+
+            var numbers = input.Split(',');
+            var total = 0;
+
+            foreach(var number in numbers)
+            {
+                total += int.Parse(number); 
+            }
+            return total;
         }
     }
 }
