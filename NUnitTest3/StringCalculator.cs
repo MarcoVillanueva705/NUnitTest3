d3fb4f51d82acfcb16c56b5285c9bce915c84b0e
marco villanueva <villanuevamarco98@gmail.com>
2020-12-30 20:34:49 -0700

    notes for test two

--- NUnitTest3/StringCalculator.cs
+++ NUnitTest3/StringCalculator.cs
@@ -6,7 +6,7 @@ namespace Tests
     {
         public int Add(string input)
         {
-            //solution for test one to pass
+            //solution for test 1 to pass
             //if the input is null or a blank string (like line 21 in Unit test)
             //if (string.IsNullOrEmpty(input))
             //    return 0;
@@ -17,6 +17,7 @@ namespace Tests
                 return 0;
             }
 
+            //this gets test 2 to pass
             var numbers = input.Split(',');
             var total = 0;
 
